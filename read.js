037a9e176af067bc6b5021d1b09f1a951d503d85
hornd24 <david.horn689@gmail.com>
2017-12-01 00:41:16 -0800

    help

--- read.js
+++ read.js
@@ -1,5 +1,9 @@
 var mysql = require("mysql");
-
+var inquirer = require('inquirer');
+var item1;
+var item2;
+var quanity1;
+var quanity2;
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -19,14 +23,58 @@ connection.connect(function(err) {
   });
 
 function displayStore(){
-    console.log('Welcome to Bamazon, The Greatest Store On the Console!');
-  connection.query("SELECT Product_Id ,Product_Name , Department_Name , Price FROM products", function(err, res) {
+    console.log('Welcome to Bamazon, The Greatest Store On the Node Console!');
+  connection.query("SELECT Product_Id ,Product_Name , Department_Name , Price FROM products ORDER BY Department_Name", function(err, res) {
     if (err) throw err;
     for(var i=0;i<res.length;i++){
     console.log(JSON.stringify(res[i]));
 
     }
-    connection.end();
+    console.log('To Buy A Product Please Enter The Product Id')
+    buyProducts1()
+    // connection.end();
   
 })
 }
+
+
+function buyProducts1(){
+    
+        inquirer.prompt([
+            {
+            
+            name:'product',
+            message:'What Product Would like to buy? '
+        },
+    {
+        name:'quanity',
+        message:'How many Would You Like To Purchase?'
+    },
+    {
+        type:'input',
+        name:'yes',
+        message:'Would you like to buy another product?Y/N'
+    }
+    
+        ]).then(function(item){
+             item1 =   item.product
+             quanity1 =item.quanity
+            if (item.yes=='y'){
+            
+            //  buyProducts();
+    
+            }else{
+                connection.query("SELECT Stock_Quantity FROM products WERE?",[item1=quanity1], function(err, res) {
+                    if (err) throw err;
+                    for(var i=0;i<res.length;i++){
+                    console.log(JSON.stringify(res[i]));
+                
+                }
+
+                
+             
+        })
+    }        
+    
+     })
+    }
